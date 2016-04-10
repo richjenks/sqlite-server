2c9f2cf40f4a305f199133ef4ceb02fbe97dc68a
Rich Jenks <rich@richjenks.com>
2016-04-10 17:27:18 +0100

    Flattened tests

---
+++ tests/ServerTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class ServerTest extends PHPUnit_Framework_TestCase {
+
+	protected function setUp() {
+		// Initialise PHP built-in server on obscure port number
+		// Use Guzzle to test API IO
+	}
+
+}
